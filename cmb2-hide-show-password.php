128358cfc3fc380b3968ee644bcaddcc5d05f797
Patrick Groot <info@patrickgroot.com>
2020-02-21 13:05:39 +0100

    Update cmb2-hide-show-password.php

--- cmb2-hide-show-password.php
+++ cmb2-hide-show-password.php
@@ -8,7 +8,7 @@
  * Version: 1.0.0
  * Author: Automex.website
  * Author URI: http://automex.website
- * License: MIT
+ * License: GPLv3
  * Text Domain: amex-hide-show-password
  * Domain Path: /languages
  */
